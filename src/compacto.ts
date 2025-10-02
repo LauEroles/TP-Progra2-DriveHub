9692e15c771a4f4dbc8023a566d6214a2710f88b
LauEroles <laura.eroles@gmail>
2025-10-01 21:30:04 -0300

    cambios this.km en Calcular Tarifa clase Compacto (sesion grupal)

--- src/compacto.ts
+++ src/compacto.ts
@@ -1,7 +1,7 @@
 
 import Reserva from "./reserva";
 import Vehiculo from "./vehiculo";
-import { KM_MAX_COMPACTO } from "./constantes";
+import { KM_MAX_COMPACTO, TARIFA_BASE_COMPACTO } from "./constantes";
 
 
 export default class Compacto extends Vehiculo {
@@ -9,7 +9,7 @@ export default class Compacto extends Vehiculo {
 
     constructor(km: number, matricula: string) {
         super(km, matricula);
-        this.tarifaBase = 30;
+        this.tarifaBase = TARIFA_BASE_COMPACTO;
         this.cargoVariable = 0.15;
 
     }
@@ -17,9 +17,9 @@ export default class Compacto extends Vehiculo {
     calcularTarifa(reserva: Reserva): number {
         //ajustar segun arreglos kilometraje?
         let total: number = this.tarifaBase;
-        let kmsRecorridos: number = this.kmFinal - this.kmInicial;
+        let kmsRecorridos: number = reserva.getKmFinal() - this.km;
         if (kmsRecorridos > KM_MAX_COMPACTO) {
-            let excedente: number = kmsRecorridos - this.kmMax;
+            let excedente: number = kmsRecorridos - KM_MAX_COMPACTO;
             total += excedente * this.cargoVariable;
         } 
         return total;

b91b0e7d5fdac8ed2f8de4de1983464197281194
Melissa <lissak230@gmail.com>
2025-11-08 19:13:25 -0300

    agrego temporada a reserva y calcular total segun temporada

--- src/reserva.ts
+++ src/reserva.ts
@@ -2,12 +2,12 @@ import Vehiculo from "./vehiculo";
 import Cliente from "./cliente";
 
 export default class Reserva {
-
     public vehiculo: Vehiculo;
     public cliente: Cliente;
     public fechaInicio: Date;
     public fechaFin: Date;
     public kmsRecorridos: number;
+    public temporada: Temporada;
 
     constructor(vehiculo: Vehiculo, cliente: Cliente, fechaInicio: Date, fechaFin: Date) {
         this.vehiculo = vehiculo;
@@ -15,6 +15,28 @@ export default class Reserva {
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
         this.kmsRecorridos = 0;
+        this.temporada = undefined as unknown as Temporada;
+    }
+
+    public getTemporada(): Temporada {
+        return this.temporada;
+    }
+
+    public setTemporada(): void {
+        let mes: number = this.fechaInicio.getMonth() + 1;
+        if (mes in  [12, 1, 2, 7]) {
+            this.temporada = new TempAlta();
+        } else if (mes in [3, 4, 11]) {
+            this.temporada = new TempMedia();
+        } else {
+            this.temporada = new TempBaja();
+        }
+    }
+
+    public calcularTotal(): number {
+        let base: number = this.temporada.calcTarifaBase(this.vehiculo.getTarifaBase());
+        let total: number = base * this.diasReserva() + this.vehiculo.getCargoFijo() + this.vehiculo.calcCargoVariable(this.getKmsRecorridos());
+        return total;
     }
 
     public validarFecha(): boolean {

24b0f1aa26485528d6b4039091898704b66bd166
Melissa <lissak230@gmail.com>
2025-11-13 23:53:13 -0300

    agrego getters y setters cliente

--- src/cliente.ts
+++ src/cliente.ts
@@ -11,6 +11,22 @@ export default class Cliente {
         this.id = id;
     }
 
+    public getNombreCompleto(): string {
+        return this.nombreCompleto;
+    }
+
+    public setNombreCompleto(nombreCompleto: string): void {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
     public solicitarReserva(fechaInicio: Date, fechaFin: Date, vehiculo: Vehiculo, sistema: SistemaEmpresa) {
         sistema.realizarReserva(vehiculo, this, fechaInicio, fechaFin);
     } 

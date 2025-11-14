37af0354ed0a26d5c3a096fcce9663549a196287
Melissa <lissak230@gmail.com>
2025-11-13 23:29:43 -0300

    fix metodo solicitar reserva de cliente

--- src/cliente.ts
+++ src/cliente.ts
@@ -1,26 +1,18 @@
-import Reserva from "./reserva";
 import SistemaEmpresa from "./sistemaEmpresa";
+import Vehiculo from "./vehiculo";
 
-
-export default class Cliente{
+export default class Cliente {
 
     private nombreCompleto: string;
     private id: number;
-    // en un futuro podriamos hacer una mejora y tener un historial de reservas de cada cliente que sea una coleccion
-    private  reserva: Reserva;
 
     constructor(nombreCompleto: string, id: number) {
         this.nombreCompleto = nombreCompleto;
         this.id = id;
-        this.reserva = undefined as unknown as Reserva;
     }
 
-    public solicitarReserva(reserva:Reserva,sistema:SistemaEmpresa){
-
-        sistema.realizarReserva(reserva);
-
+    public solicitarReserva(fechaInicio: Date, fechaFin: Date, vehiculo: Vehiculo, sistema: SistemaEmpresa) {
+        sistema.realizarReserva(vehiculo, this, fechaInicio, fechaFin);
     } 
-
-
 
 }

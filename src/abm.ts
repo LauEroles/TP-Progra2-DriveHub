f795d99562b0a0fcee8421e76242a719da6541b5
Melissa <lissak230@gmail.com>
2025-11-15 15:30:05 -0300

    coments jsdoc abm

--- src/abm.ts
+++ src/abm.ts
@@ -1,4 +1,16 @@
 export default interface ABM {
+    /**
+    * Agrega un ítem a una lista.
+    * @template T Tipo del ítem y de los elementos de la lista.
+    * @param {T} item Ítem a agregar.
+    * @param {Array<T>} lista Lista donde se agregará el ítem.
+    */
     agregar<T>(item: T, lista: Array<T>): void;
+    /**
+    * Elimina un ítem de una lista.
+    * @template T Tipo del ítem y de los elementos de la lista.
+    * @param {T} item Ítem a eliminar.
+    * @param {Array<T>} lista Lista de donde se eliminará el ítem.
+    */
     eliminar<T>(item: T, lista: Array<T>): void;
 }

23850624b1aafa6e3bd134a704220bd5377c03a5
Melissa <lissak230@gmail.com>
2025-11-15 16:09:58 -0300

    coments jsdoc gestor mantenimiento

--- src/gestorMantenimiento.ts
+++ src/gestorMantenimiento.ts
@@ -1,8 +1,16 @@
 import MantenimientoVehiculo from "./mantenimientoVehiculo";
 import Vehiculo from "./vehiculo";
 
+/**
+* Gestor encargado de registrar mantenimientos en los vehículos.
+*/
 export default class GestorMantenimiento {
 
+    /**
+    * Registra un mantenimiento en un vehículo.
+    * @param {Vehiculo} v Vehículo al que se le registrará el mantenimiento.
+    * @param {MantenimientoVehiculo} m Mantenimiento a registrar.
+    */
     public registrarMantenimiento (v: Vehiculo, m: MantenimientoVehiculo): void {
         v.agregarMantenimientoVehiculo(m); 
     }
